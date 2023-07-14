f46bafbcb33ae068abaf431a347e7f4c32bca25b
Tony Howell <anhwell@amazon.com>
2023-07-14 06:24:10 -0500

    fixes

--- lib/streamlit-docker/main.py
+++ lib/streamlit-docker/main.py
@@ -60,7 +60,7 @@ Do not include any framing language such as "According to the context" in your r
 
 Simply answer the question clearly and with lots of detail using only the relevant details from the information below. If the context does not contain the answer, say "I don't know."
 I repeat DO NOT TALK ABOUT THE CONTEXT
-Now read this context and answer the question at the bottom:
+Now read this context and answer the question at the bottom. Make it fun and use emojis where appropriate.
 Context: {context}"
 
 Question: "Hey """ + customer_name + """ Chatbot! {question}
@@ -84,9 +84,6 @@ if chatbot_logo:
 
 st.subheader(customer_name + " ChatBot",)
 
-
-
-
 chain_type_kwargs = {"prompt": PROMPT}
 qa = ConversationalRetrievalChain.from_llm(
     llm=llm, 
@@ -95,9 +92,6 @@ qa = ConversationalRetrievalChain.from_llm(
     return_generated_question=True,          # ☜ ANSWER
     condense_question_prompt=CONDENSE_QUESTION_PROMPT,
     verbose=True,
-
-
-
 )
 
 qa.combine_docs_chain.llm_chain.prompt = PROMPT
@@ -119,9 +113,6 @@ def submit():
     st.session_state['input'] = ""
 
 
-
-
-
 user_input = st.session_state['user_input'] if 'user_input' in st.session_state else None
 
 if user_input:
@@ -129,27 +120,29 @@ if user_input:
     # output = chain.run(input=user_input)
     result = qa({"question":user_input, "chat_history": st.session_state["chat_history"]})
     logger.info(result)
-    if("I don't know" not in result["answer"] and len(result['source_documents']) > 0):
-        source_url = result['source_documents'][0].metadata['source']
-        safe_source_url = urllib.parse.quote_plus(source_url)
-        response_text = result["answer"].strip() + "\n" + source_url
-        # chat_history = [(user_input, result["answer"])]
-        # st.session_state["chat_history"].append((user_input, result["answer"]+ "Source Document Text: " + result['source_documents'][0].page_content))
+    if("I apologize" not in result ["answer"] and "I don't know" not in result["answer"] and len(result['source_documents']) > 0):
+        print(result['source_documents'])
+        response_text = """
+{}
+
+You might find these links helpful:
+
+""".format(result["answer"].strip())
+        for source in result['source_documents']:
+            if source.metadata['title'] not in response_text:
+                response_text += f"[{source.metadata['title']}]({source.metadata['source']})\n"
+        
+        logger.info(response_text)
         st.session_state["chat_history"].append((user_input, result["answer"]))
     else:
         response_text = "Sorry, I don't know the answer to that question."
     logger.info("Condensed query: " + result["generated_question"])
     logger.info("Response text: " + response_text)
-    st.session_state.generated.append(response_text)
+    st.session_state.generated.append(f'{response_text}')
     st.session_state.condensed_query = result["generated_question"]
     #remove old chat history older than 2 messages
     if len(st.session_state["chat_history"]) > 2:
         st.session_state["chat_history"].pop(0)
-
-
-
-
-
 
 
 if st.session_state["generated"]:
@@ -157,8 +150,6 @@ if st.session_state["generated"]:
         index = len(st.session_state["generated"]) - i - 1
         message(st.session_state["past"][index], is_user=True, key=str(index) + "_user")
         message(st.session_state["generated"][index], key=str(index), logo=chatbot_logo, avatar_style="no-avatar")
-
-
 
 st.text_input(label="You: ", key="input", value="", on_change=submit, placeholder="Ask a question!", )
 st.write("")
@@ -171,11 +162,3 @@ st.write("")
 st.write("")
 with st.expander("Debug", expanded=False):
     st.write(st.session_state)
-    st.text_input(label="access_key_id", key="access_key_id", value="", placeholder="access_key_id")
-    st.text_input(label="secret_key", key="secret_key", value="", placeholder="secret_key")
-    st.text_input(label="session_token", key="session_token", value="", placeholder="session_token")
-
-
-
-
-
